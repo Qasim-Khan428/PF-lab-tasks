4a136ab1f53286becb442f87594051ba86e8a1a0
Qasim-Khan428 <m.qasimkhan428@gmail.com>
2026-09-06 20:56:18 +0500

    Add files via upload

---
+++ q4.c
@@ -0,0 +1,8 @@
+#include <stdio.h>
+int main() {
+    float Temperature;
+    printf("Enter the temperature in Celsius: ");
+    scanf("%f", &Temperature);
+    printf(" Temperature: %.2f", Temperature);
+    return 0;
+}
