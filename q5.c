fc141b2ceb6311e46c0fe0aea2c447608f188a4b
Qasim-Khan428 <m.qasimkhan428@gmail.com>
2026-09-06 21:13:24 +0500

    Add files via upload

---
+++ q5.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+int main()
+{
+    printf("\tName\tAge\tGrade");
+    printf("\n\tAli\t20\tA");
+    printf("\n\tSara\t21\tB");
+    printf("\n\tAhmed\t19\tA");
+    return 0;
+}
