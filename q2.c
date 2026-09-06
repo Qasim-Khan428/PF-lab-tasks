5623be30a72f803495ed25342472d97bb747f4d9
Qasim-Khan428 <m.qasimkhan428@gmail.com>
2026-09-06 19:17:24 +0500

    Add files via upload

---
+++ q2.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+int main(){
+    int num1, num2;
+    printf("Enter first number: ");
+    scanf("%d", &num1);
+    printf("Enter second number: ");
+    scanf("%d", &num2);
+    printf("first number:%d\n",num1);
+    printf("second number:%d\n",num2);
+    return 0;
+}
