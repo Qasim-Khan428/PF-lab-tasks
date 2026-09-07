70620e32cadb9b2b7a923b1b396c35add405b33e
Qasim-Khan428 <m.qasimkhan428@gmail.com>
2026-09-07 20:22:17 +0500

    Add files via upload

---
+++ q6.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+int main() {
+    printf("Name:");
+    printf("\t\tAli");
+    printf("\nAge:");
+    printf("\t\t20");
+    printf("\nGrade:");
+    printf("\t\tA");
+    printf("\nFile path:C:\\users\\Students");
+    printf("\n\n\nHe said \"Hello\"");
+    return 0;
+
+}
